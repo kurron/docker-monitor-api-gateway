7a95f380e16988ce2d4b5c9c2a8530db3696f5e0
Ron Kurr <kurr@kurron.org>
2015-09-18 14:33:27 +0000

    Noted why using host networking is preferable

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --detach=true --hostname=api-gateway --name=api-gateway --publish=8000:8000 --add-host=rabbitmq:10.0.2.15 kurron/monitor-api-gateway:latest 
+# Use host networking to avoid having to map every hostname needed for the simulation 
+docker run --detach=true --name=api-gateway --net=host --publish=8000:8000 kurron/monitor-api-gateway:latest 
 docker logs --follow=true --tail=all api-gateway
