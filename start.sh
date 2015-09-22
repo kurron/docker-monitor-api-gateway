fccd2d100314b887ab791220bc1a615bf3841cf1
Ron Kurr <kurr@kurron.org>
2015-09-22 21:34:13 +0000

    New memory settings

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 # Use host networking to avoid having to map every hostname needed for the simulation 
 docker run --detach=true --name=api-gateway --net=host --publish=8000:8000 kurron/monitor-api-gateway:latest 
-docker logs --follow=true --tail=all api-gateway
+docker logs --follow=true --tail=25 api-gateway
